ec5c6bf1e17c2afdc507528ae37618130ce9bf77
Your Name <Tsenguun_7595@yahoo.com>
2022-06-28 14:12:35 +0800

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+// value utga
+
+//33;
+//123;
+//13.4;
+
+//"Hello world" String type
+// '' single quote
+// "" double quote
+//true 
+// false
+
+// [1,3,6] array buyu husnegt daraalaltai ugugdliig 0 ees ehelj toolno
+//{name:"bat"} object
+
+// Null hooson
+
+// Undefined  бүр байхгүй  хоосон утга.
+
+let x=3.14;
+let y=3;
+document.getElementById("demo").innerHTML = x + "<br>" + y;
